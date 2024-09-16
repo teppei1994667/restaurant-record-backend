0407143edad80fb3b52ec18b88a781feed3f7985
teppei1994667 <teppei.engineer.667@gmail.com>
2024-09-16 23:01:25 +0900

    ユーザーの認証確認時の返却データ 変更

--- app/controllers/api/v1/users_controller.rb
+++ app/controllers/api/v1/users_controller.rb
@@ -2,6 +2,6 @@ class Api::V1::UsersController < ApplicationController
   before_action :authenticate_api_v1_user!
 
   def authenticated
-    render json: { is_login: true, data: current_api_v1_user }
+    render json: { data: current_api_v1_user }
   end
 end
