ec551a7031916650a54d6a5f582043319c4b2db5
teppei1994667 <teppei.engineer.667@gmail.com>
2024-12-29 14:03:47 +0900

    Storeモデルを作成

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,9 +1,7 @@
-tests = [
-  ["test", "test@gmail.com", "Test11111111"],
-  ["test2", "test2@gmail.com", "Test22222222"],
-  ["test3", "test3@gmail.com", "Test33333333"],
+stores = [
+  [15, "テスト店", "0699999999", "大阪市西区立売堀6-6-6 朝日プラザ長堀西", 32, 20]
 ]
 
-tests.each do |name, email, password|
-  Test.create(name: name, email: email, password: password)
+stores.each do |user_id, store_name, telephone_number, address, seating_capacity, area|
+  Store.create(user_id: user_id, store_name: store_name, telephone_number: telephone_number, address: address, seating_capacity: seating_capacity, area: area)
 end
