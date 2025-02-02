8c13072d0290b04d45362241f910954930f6bf7b
teppei1994667 <teppei.engineer.667@gmail.com>
2025-02-02 18:20:27 +0900

    ユーザーの削除機能実装

--- app/controllers/api/v1/auth/registrations_controller.rb
+++ app/controllers/api/v1/auth/registrations_controller.rb
@@ -1,11 +1,19 @@
 class Api::V1::Auth::RegistrationsController < DeviseTokenAuth::RegistrationsController
-  before_action :authenticate_api_v1_user!, only: [:update]
+  before_action :authenticate_api_v1_user!, only: [:update, :destroy]
   # DeviseTokenAuth::RegistrationsControllerで定義されたcreateメソッド実行後にset_token_infoメソッドを実行
   after_action :set_token_info, only: [:create]
 
   def update
     if current_api_v1_user.update(account_update_params)
       render json: {user_model: current_api_v1_user}
+    else
+      render json: { errors: current_api_v1_user.errors.full_messages }, status: :unprocessable_entity
+    end
+  end
+
+  def destroy
+    if current_api_v1_user.destroy
+      render json: { message: "アカウントを削除しました" }
     else
       render json: { errors: current_api_v1_user.errors.full_messages }, status: :unprocessable_entity
     end
